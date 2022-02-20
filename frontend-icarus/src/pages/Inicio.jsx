155b69849228ef990bb3b272503fee8e74f4039e
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-02-19 20:47:09 -0500

    añadir estilos

--- frontend-icarus/src/pages/Inicio.jsx
+++ frontend-icarus/src/pages/Inicio.jsx
@@ -1,41 +1,109 @@
-import React from 'react'
+import React from "react";
+//importar css de la carpeta assets css styles
+import ".../assets/css/styles.css";
 
 //componentes
-import Navbar from '../components/Navbar'
-import Carousel from '../components/Carousel'
+import Navbar from "../components/Navbar";
 
 const Inicio = () => {
   return (
     <>
-      <Navbar />
-      <Carousel />
-      <div class="container marketing">
-      <div class="row">
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+      <div class="bg-dark">
+        <Navbar />
+        <div class="container marketing">
+          <div class="row">
+            <div class="col-lg-4">
+              <svg
+                class="bd-placeholder-img rounded-circle"
+                width="140"
+                height="140"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: 140x140"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect>
+                <text x="50%" y="50%" fill="#777" dy=".3em">
+                  140x140
+                </text>
+              </svg>
 
-        <h2>Heading</h2>
-        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-        <p><a class="btn btn-secondary" href="#">View details »</a></p>
-      </div>
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+              <h2>Heading</h2>
+              <p>
+                Some representative placeholder content for the three columns of
+                text below the carousel. This is the first column.
+              </p>
+              <p>
+                <a class="btn btn-secondary" href="#">
+                  View details »
+                </a>
+              </p>
+            </div>
+            <div class="col-lg-4">
+              <svg
+                class="bd-placeholder-img rounded-circle"
+                width="140"
+                height="140"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: 140x140"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect>
+                <text x="50%" y="50%" fill="#777" dy=".3em">
+                  140x140
+                </text>
+              </svg>
 
-        <h2>Heading</h2>
-        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-        <p><a class="btn btn-secondary" href="#">View details »</a></p>
-      </div>
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+              <h2>Heading</h2>
+              <p>
+                Another exciting bit of representative placeholder content. This
+                time, we've moved on to the second column.
+              </p>
+              <p>
+                <a class="btn btn-secondary" href="#">
+                  View details »
+                </a>
+              </p>
+            </div>
+            <div class="col-lg-4">
+              <svg
+                class="bd-placeholder-img rounded-circle"
+                width="140"
+                height="140"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: 140x140"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect>
+                <text x="50%" y="50%" fill="#777" dy=".3em">
+                  140x140
+                </text>
+              </svg>
 
-        <h2>Heading</h2>
-        <p>And lastly this, the third column of representative placeholder content.</p>
-        <p><a class="btn btn-secondary" href="#">View details »</a></p>
+              <h2>Heading</h2>
+              <p>
+                And lastly this, the third column of representative placeholder
+                content.
+              </p>
+              <p>
+                <a class="btn btn-secondary" href="#">
+                  View details »
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Inicio
+export default Inicio;
