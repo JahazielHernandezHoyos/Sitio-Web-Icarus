01f3296f1025922add178862a130e0ef0cbfc5f2
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-02-19 23:30:42 -0500

    insertar elementos que utilizare mas adelante

--- frontend-icarus/src/pages/Inicio.jsx
+++ frontend-icarus/src/pages/Inicio.jsx
@@ -12,7 +12,7 @@ const Inicio = () => {
           <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
               <h1>Icarus <h1 style={{color: "#f2d027"}}>Solutions</h1></h1>
-              <p class="py-5 lead">
+              <p class="py-5 lead text-sec">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur ad mollitia doloremque, doloribus pariatur molestias consequatur tempora aliquam quo ipsa deleniti commodi laboriosam odio. Tempore consequuntur iure totam error.
               </p>
               <p class="lead">
@@ -26,97 +26,85 @@ const Inicio = () => {
             </div>
           </div>
         </section>
-        <div class="mt-5 container marketing">
-          <div class="row">
-            <div class="col-lg-4">
-              <svg
-                class="bd-placeholder-img rounded-circle"
-                width="140"
-                height="140"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: 140x140"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#777"></rect>
-                <text x="50%" y="50%" fill="#777" dy=".3em">
-                  140x140
-                </text>
-              </svg>
 
-              <h2>Heading</h2>
-              <p>
-                Some representative placeholder content for the three columns of
-                text below the carousel. This is the first column.
-              </p>
-              <p>
-                <a class="btn btn-secondary" href="#">
-                  View details »
-                </a>
-              </p>
-            </div>
-            <div class="col-lg-4">
-              <svg
-                class="bd-placeholder-img rounded-circle"
-                width="140"
-                height="140"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: 140x140"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#777"></rect>
-                <text x="50%" y="50%" fill="#777" dy=".3em">
-                  140x140
-                </text>
-              </svg>
+        <img src="https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg" alt="" />
 
-              <h2>Heading</h2>
-              <p>
-                Another exciting bit of representative placeholder content. This
-                time, we've moved on to the second column.
-              </p>
-              <p>
-                <a class="btn btn-secondary" href="#">
-                  View details »
-                </a>
-              </p>
-            </div>
-            <div class="col-lg-4">
-              <svg
-                class="bd-placeholder-img rounded-circle"
-                width="140"
-                height="140"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: 140x140"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#777"></rect>
-                <text x="50%" y="50%" fill="#777" dy=".3em">
-                  140x140
-                </text>
-              </svg>
-
-              <h2>Heading</h2>
-              <p>
-                And lastly this, the third column of representative placeholder
-                content.
-              </p>
-              <p>
-                <a class="btn btn-secondary" href="#">
-                  View details »
-                </a>
-              </p>
-            </div>
-          </div>
+        <div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="https://cdn.pixabay.com/photo/2017/03/17/06/27/seo-2151033_960_720.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">Design responsive</h1>
+        <p class="lead py-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error alias quidem tenetur accusamus aut quod fugiat id est dolorum, recusandae reiciendis fuga iusto numquam tempore eos necessitatibus at nam consectetur.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Hire</button>
         </div>
+      </div>
+    </div>
+  </div>
+
+        <div class="container px-4 py-5" id="icon-grid">
+    <h2 class="pb-2 border-bottom">Icon grid</h2>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#bootstrap"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#cpu-fill"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#calendar3"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#home"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#speedometer2"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#toggles2"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#geo-fill"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#tools"></use></svg>
+        <div>
+          <h4 class="fw-bold mb-0">Featured title</h4>
+          <p>Paragraph of text beneath the heading to explain the heading.</p>
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
     </>
   );
