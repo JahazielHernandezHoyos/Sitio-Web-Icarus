afee2e0149a0cb91433217bcbf0efb6d81f2064f
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-02-20 17:58:36 -0500

    Eliminacion de cosas que no usaba

--- frontend-icarus/src/pages/Inicio.jsx
+++ frontend-icarus/src/pages/Inicio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //componentes
 import Navbar from "../components/Navbar";
+import Services from "./sections/Services";
 
 const Inicio = () => {
   return (
@@ -11,100 +12,30 @@ const Inicio = () => {
         <section class="text-center container">
           <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
-              <h1>Icarus <h1 style={{color: "#f2d027"}}>Solutions</h1></h1>
+              <h1>
+                Icarus <h1 style={{ color: "#f2d027" }}>Solutions</h1>
+              </h1>
               <p class="py-5 lead text-sec">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur ad mollitia doloremque, doloribus pariatur molestias consequatur tempora aliquam quo ipsa deleniti commodi laboriosam odio. Tempore consequuntur iure totam error.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                consectetur ad mollitia doloremque, doloribus pariatur molestias
+                consequatur tempora aliquam quo ipsa deleniti commodi laboriosam
+                odio. Tempore consequuntur iure totam error.
               </p>
               <p class="lead">
-                    <button href="#" class="me-3 btn btn-primary button-text">
-                      Contact
-                    </button>
-                  <button href="#" class="ms-3 btn btn-secondary button-text">
-                    View on Github
-                  </button>
+                <button href="#" class="me-3 btn btn-primary button-text">
+                  Contact
+                </button>
+                <button href="#" class="ms-3 btn btn-secondary button-text">
+                  View on Github
+                </button>
               </p>
             </div>
           </div>
         </section>
 
-        <img src="https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg" alt="" />
+        <Services/>
+        
 
-        <div class="container col-xxl-8 px-4 py-5">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <img src="https://cdn.pixabay.com/photo/2017/03/17/06/27/seo-2151033_960_720.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
-      </div>
-      <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3">Design responsive</h1>
-        <p class="lead py-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error alias quidem tenetur accusamus aut quod fugiat id est dolorum, recusandae reiciendis fuga iusto numquam tempore eos necessitatibus at nam consectetur.</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Hire</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
-        <div class="container px-4 py-5" id="icon-grid">
-    <h2 class="pb-2 border-bottom">Icon grid</h2>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#bootstrap"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#cpu-fill"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#calendar3"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#home"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#speedometer2"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#toggles2"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#geo-fill"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use href="#tools"></use></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Paragraph of text beneath the heading to explain the heading.</p>
-        </div>
-      </div>
-    </div>
-  </div>
       </div>
     </>
   );
