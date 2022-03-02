59427a71fdf48ed5be4168921f274849699ca07a
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-03-02 14:16:25 -0500

    footer con redes

--- frontend-icarus/src/components/Footer.jsx
+++ frontend-icarus/src/components/Footer.jsx
@@ -1,10 +1,16 @@
-const Footer = () => {
+import githubIMG from "../assets/img/github.svg";
+import instagramIMG from "../assets/img/instagram.svg";
 
+const Footer = () => {
   //pedir año actual para el copyright
   const year = new Date().getFullYear();
 
   return (
-    <footer className="py-5 container">
+    <footer className="sticky-bottom py-5 container">
+      <div className="mx-auto py-3" style={{ width: "90px" }}>
+        <img className="px-1" src={githubIMG} href="/"/>
+        <img className="px-1" src={instagramIMG} href="/"/>
+      </div>
       <p className="text-center lead">
         © {year} Icarus Solutions. Todos los derechos reservados.
         {/* <a href="#">Privacy</a> ·{" "}
