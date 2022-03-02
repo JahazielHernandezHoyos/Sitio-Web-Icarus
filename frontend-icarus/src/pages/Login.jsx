2ed2ce214361d8c8fed999ab1a2a1f704fa445f1
PaperlinkAbyss <96418397+PaperlinkAbyss@users.noreply.github.com>
2022-03-02 22:23:38 +0100

    Update Login.jsx

--- frontend-icarus/src/pages/Login.jsx
+++ frontend-icarus/src/pages/Login.jsx
@@ -2,10 +2,10 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const login = () => {
+const login = ({language, setLanguage}) => {
   return (
     <>
-      <Navbar />
+      <Navbar language={language} setLanguage={setLanguage}/>
       <div className="mt-5 py-5 col-md-10 mx-auto col-lg-5">
         <form className="bg-dark p-4 p-md-5 border rounded-3 bg-light">
           <div className="form-floating mb-3">
@@ -24,22 +24,26 @@ const login = () => {
               id="floatingPassword"
               placeholder="Contraseña"
             />
-            <label for="floatingPassword">Contraseña</label>
+            <label for="floatingPassword">{language === "es"? "Contraseña" 
+                                  : "Password"}</label>
           </div>
           <div className="checkbox mb-3">
             <label>
-              <input type="checkbox" value="remember-me" /> Recordar contraseña
+              <input type="checkbox" value="remember-me" /> {language === "es"? "Recordar contraseña" 
+                                  : "Remember password"}
             </label>
           </div>
           <button className="w-100 btn btn-lg btn-primary" type="submit">
-            Registrarse
+            {language === "es" ? "Registrarse": "Register"}
           </button>
           <small className="my-4 text-muted">
-            Al hacer clic en Registrarse, acepta los términos de uso.
+            
+            {language === "es"? "Al hacer clic en Registrarse, acepta los términos de uso." 
+                                  : ""}
           </small>
         </form>
       </div>
-      <Footer />
+      <Footer language={language} />
     </>
   );
 };
