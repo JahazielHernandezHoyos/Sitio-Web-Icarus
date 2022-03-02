aa96892a22e14d37a184198ba2a925e207200aea
PaperlinkAbyss <96418397+PaperlinkAbyss@users.noreply.github.com>
2022-03-02 22:22:19 +0100

    Update App.js

--- frontend-icarus/src/App.js
+++ frontend-icarus/src/App.js
@@ -6,15 +6,21 @@ import {
 } from 'react-router-dom';
 
 //paginas
-import Inicio from './pages/Inicio';
-import Login from './pages/Login';
+import ENInicio from './translations/en/pages/Inicio';
+import ESInicio from './translations/es/pages/Inicio';
+import Login from './translations/es/pages/Login';
 
 function App() {
+ const [language, setLanguage] = React.useState("es")
+ const setLanguage = (languageString) => {
+   setLanguage(languageString)
+ }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ESInicio language={language} language={language} setLanguage={setLanguage}/>}  />
+        {/* <Route path="/en" element={<ENInicio />} /> */}
+        <Route path="/login" element={<Login language={language} setLanguage={setLanguage} />} />
       </Routes> 
     </BrowserRouter>
   );
